8c6599ed1bc9abb7277ed349d8347cd3369ac91d
Jiyeong Jeong <8533283@naver.com>
2023-09-25 18:47:33 +0900

    9/25 시나브로 자바스크립트 ep01 - 4

--- JavaScript/시나브로자바스크립트/ep01/main.js
+++ JavaScript/시나브로자바스크립트/ep01/main.js
@@ -1,47 +1,23 @@
+// innerHTML and its inefficiency
+
 document.querySelector('#app').innerHTML = `
-<button type="button" class="hello1">hello1</button>
-<button type="button" class="hello2">hello2</button>
-<button type="button" class="hello3">hello3</button>
-
-<div>
-  <input class="name" type="text" placeholder="Type your name:" />
-</div>
-
-<div class="parent-of-button">
-  <button class="helloworld-button" type="button">
-    <span>Hello</span>
-    <span>World</span>
-  </button>
-</div>
+<input />
+<button>Click</button>
 `;
 
-document.querySelector('button').addEventListener('click', function (event) {
-  const input = document.querySelector('.name');
-  console.log(input.value);
+document.querySelector('button').addEventListener('click', () => {
+  const currentValue = document.querySelector('input').value;
+
+  document.querySelector('input').value = currentValue + '*';
 });
 
-document.querySelector('.name').addEventListener('input', (event) => {
-  console.log(event.target.value);
-});
+let count = 0;
 
-// 이벤트 버블링
-document
-  .querySelector('.helloworld-button')
-  .addEventListener('click', (event) => {
-    event.stopPropagation();
-    console.log('event from button', event);
-  });
-
-document
-  .querySelector('.parent-of-button')
-  .addEventListener('click', (event) => {
-    console.log('event from div', event);
-  });
-
-document.querySelector('.name').addEventListener('keyup', (event) => {
-  console.log('keyup', event);
-});
-
-document.body.addEventListener('keyup', (event) => {
-  console.log(event.key);
-});
+setInterval(() => {
+  count += 1;
+  document.querySelector('#app').innerHTML = `
+    <input />
+    <button>Click</button>
+    <p>count:${count}</p>
+  `;
+}, 5000);
